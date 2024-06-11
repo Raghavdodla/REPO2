7db9feff7cab87064f42be2f85a50fc45ef79661
Raghavdodla <82253529+Raghavdodla@users.noreply.github.com>
2024-06-11 04:55:08 +0000

    Add fourth file

--- main.sh
+++ main.sh
@@ -1,8 +1,3 @@
-echo "# REPO1" >> README.md
-git init
-echo hi > file && git add . && git commit -m "Add first file"
-echo hi > file2 && git add . && git commit -m "Add second file"
-echo hi > file3 && git add . && git commit -m "Add third file"
-git branch -M main
-git remote add origin https://github.com/Raghavdodla/REPO2.git
-git push -u origin main
+echo hi > file4 && git add . && git commit -m "Add fourth file"
+echo hi > file5 && git add . && git commit -m "Add fifth file"
+git push
